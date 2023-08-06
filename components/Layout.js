ef38a1213581e1462abf04e9421a56dd3d719826
Mateus Cardoso <cardosorocha27@hotmail.com>
2023-08-06 20:25:54 -0300

    Navbar Full

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,30 @@
-const Layout = () => {
-  return <div>Layout</div>;
+//Fontes
+import { Sora } from '@next/font/google'
+
+//Configs Fonte
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
+
+
+//Componentes
+import Nav from '../components/Nav'
+import Header from '../components/Header'
+import TopLeftImg from '../components/TopLeftImg'
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+
+  )
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
 
+
+//Componentes
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp;
