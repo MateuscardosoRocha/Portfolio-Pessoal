fe0893c10d1326a169f2cc50f5cb9a78d4d6434b
Mateus Cardoso <cardosorocha27@hotmail.com>
2023-08-06 21:12:02 -0300

    Transition

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,25 @@ import '../styles/globals.css';
 
 //Componentes
 import Layout from '../components/Layout';
-import Nav from '../components/Nav';
+import Transition from '../components/Transition';
 
+
+import { useRouter } from 'next/router'
+import { AnimatePresence, motion } from 'framer-motion'
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+
+        </motion.div>
+
+      </AnimatePresence>
     </Layout>
   )
 }
